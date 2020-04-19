cb881fbb770422cf71e30da9fce9e9a6be490774
ShamyratDauren <57912944+ShamyratDauren@users.noreply.github.com>
2020-04-19 18:58:40 +0600

    Create hw#6.java

---
+++ hw#6.java
@@ -0,0 +1,33 @@
+package com.company;
+/*
+Create abstract method in class Employee overall_income() which should return overall income.
+And then in each extended (from Employee) class calculate overall income and then print out.
+ */
+
+abstract class Employee{
+    abstract int overall_income();
+}
+
+class S1 extends Employee{
+    public int salary = 200000;
+
+    int overall_income() {
+        return salary;
+    }
+}
+
+class S2 extends Employee {
+    public int salary = 50000;
+
+    int overall_income() {
+        return salary;
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        S1 a = new S1();
+        S2 b = new S2();
+        System.out.print("Overall income: "+(a.overall_income() + b.overall_income()));
+    }
+}

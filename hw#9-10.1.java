184eab634315822de5a2846ed541abd9a068d699
ShamyratDauren <57912944+ShamyratDauren@users.noreply.github.com>
2020-04-29 23:14:38 +0600

    Update hw#9-10.1.java

--- hw#9-10.1.java
+++ hw#9-10.1.java
@@ -13,29 +13,29 @@ import java.sql.SQLException;
 
 public class Insert {
 
-	public static void main(String[] args) {
-		String dbURL = "jdbc:mysql://localhost:3306/sampledb";
-		String username = "root";
-		String password = "dauren2000";
-		
-		try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
-			
-			String sql = "INSERT INTO Users (username, password, fullname, email) VALUES (?, ?, ?, ?)";
-			
-			PreparedStatement statement = conn.prepareStatement(sql);
-			statement.setString(1, "ahAsel");
-			statement.setString(2, "dlove11122016");
-			statement.setString(3, "AkhmetovaAsel");
-			statement.setString(4, "a.akhmetova@gmail.com");
-			
-			int rowsInserted = statement.executeUpdate();
-			if (rowsInserted > 0) {
-				System.out.println("A new user was inserted successfully!");
-			}
+    public static void main(String[] args) {
+        String dbURL = "jdbc:mysql://localhost:3306/sampledb";
+        String username = "root";
+        String password = "dauren2000";
 
-			
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}		
-	}
+        try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
+
+            String sql = "INSERT INTO Users (username, password, fullname, email) VALUES (?, ?, ?, ?)";
+
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, "dlove09092017");
+            statement.setString(2, "Mustafina Karina");
+            statement.setString(3, "k.mustafina@gmail.com");
+            statement.setString(4, "muKarina");
+
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new user was inserted successfully!");
+            }
+
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
